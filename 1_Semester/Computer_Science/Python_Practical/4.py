fa5be48569fdad086975fac9684f4d19edd9ea41
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2023-09-30 17:17:52 +0530

    Update 4.py

--- 1_Semester/Computer_Science/Python_Practical/4.py
+++ 1_Semester/Computer_Science/Python_Practical/4.py
@@ -1,4 +1,3 @@
-
 """Consider a tuple t1=(1, 2, 5, 7, 9, 2, 4, 6, 8, 10). WAP to perform following operations:
     a) Print half the values of the tuple in one line and the other half in the next line.
     b) Print another tuple whose values are even numbers in the given tuple.
