0f81248be29baf7b0888307c3921ac0e33051635
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2023-09-30 17:28:28 +0530

    Update 6.py

--- 1_Semester/Computer_Science/Python_Practical/6.py
+++ 1_Semester/Computer_Science/Python_Practical/6.py
@@ -1,1 +1,8 @@
 
+#11. Write a function that prints a dictionary where the keys are numbers between 1 and 5 and the values are cubes of the keys
+def make_dct():
+    dict={}
+    for i in range(1,6):
+        dict[i]=i**3
+    print(dict)
+make_dct()
