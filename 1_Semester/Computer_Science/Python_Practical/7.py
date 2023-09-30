1005654ed783072246e9300121b73def48228a24
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2023-09-30 17:51:54 +0530

    Update 7.py

--- 1_Semester/Computer_Science/Python_Practical/7.py
+++ 1_Semester/Computer_Science/Python_Practical/7.py
@@ -1,1 +1,14 @@
-
+'''8. WAP to create a list of the cubes of only the even integers appearing in the input list (mayhave elements of other types also) using the following:
+    a. 'for' loop
+    b. list comprehension'''
+input_lst=list(map(int,input("Enter the number of list with ',' ::").split(',')))
+def by_loop():
+    lst=[]
+    for i in input_lst:
+        if i%2==0:
+            lst.append(i**3)
+    print("List for all even number cube from the given or input list",lst)
+def by_comp():
+    lst=[i**3 for i in input_lst if i%2==0]
+    print(lst)
+by_comp()
