e6c2d47c96ba9c8989a389e49f52c0b7c94912e3
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2023-10-24 14:55:47 +0530

    Create 13.py

---
+++ 1_Semester/Computer_Science/Python_Practical/13.py
@@ -0,0 +1,1 @@
+# Write A Program to define a class Point with coordinates x and y as attributes. Create relevant methods and print the objects. Also define a method distance to calculate the distance between any two point objects.
