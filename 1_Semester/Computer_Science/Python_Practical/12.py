b9e12931602fd90bdf1944a4d76ab1eb5494c959
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2023-10-24 14:53:54 +0530

    Update 12.py

--- 1_Semester/Computer_Science/Python_Practical/12.py
+++ 1_Semester/Computer_Science/Python_Practical/12.py
@@ -1,1 +1,2 @@
+# Write a function that accepts two strings and returns the indices of all the occurrences of the second string in the first string as a list. If the second string is not present in the first string then it should return -1.
 
