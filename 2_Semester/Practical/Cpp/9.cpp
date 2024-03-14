224ac4b297d0b269ba475646151710c5d3a5a48c
Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>
2024-03-14 07:23:15 +0530

    Update 9.cpp

--- 2_Semester/Practical/Cpp/9.cpp
+++ 2_Semester/Practical/Cpp/9.cpp
@@ -1,5 +1,4 @@
-/*9. Define a class Person having name as a data member. Inherit two classes Student and Employee from Person. Student has additional attributes as course, marks and year and Employee has department and salary. Write display() method in all the three classes to display the corresponding attributes.Provide the necessary methods to show runtime
-polymorphism.*/
+/*9. Define a class Person having name as a data member. Inherit two classes Student and Employee from Person. Student has additional attributes as course, marks and year and Employee has department and salary. Write display() method in all the three classes to display the corresponding attributes.Provide the necessary methods to show runtime polymorphism.*/
 
 #include <iostream>
 #include <string>
